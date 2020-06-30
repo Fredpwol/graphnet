06ba10edcfa7940e708f2ec38cd855967df25261
Fredpwol <cyborg290@gmail.com>
2020-06-30 12:30:37 +0100

    modified the get method and renamed update_value to enqeue

--- utils.py
+++ utils.py
@@ -6,9 +6,12 @@ import random
 class GraphPriorityQueue:
 
     def __init__(self, graph, type="min", state=False):
-        self.type = type
+        if type == 'min' or type == 'max':
+            self.type = type
+        else:
+            raise TypeError("type attribute most be 'min' or 'max'")
         self.graph = graph
-        self.__top = float('inf')        
+        self._top = float('inf') if self.type == 'min' else -float('inf')       
         self.queue = dict()
         self.__state = state
         for node in self.graph:
@@ -20,49 +23,48 @@ class GraphPriorityQueue:
         
 
     def __repr__(self):
-        return str(list(self.queue.values))
+        return str(list(self.queue.values()))
 
 
 
     def get(self):
-        if len(self.queue != 0):
-            res = 0
+        if self.__state:
+            if all(self.status.values()):
+                return None
+        if len(self.queue) != 0:
             sign = "<=" if self.type == "min" else ">=" if self.type == "max" else ''
-            if not self.__state:
-                for node in self.graph:
-                    if eval(f"self.queue[{node}] {sign} self.__top"):
-                        self.__top = self.queue[node]
-                        res = node
-            else:
-                for node in self.graph:
-                    if eval(f"self.queue[{node}] {sign} self.__top) and (not self.status[{node}]"):
-                        self.__top = self.queue[node]
-                        res = node
+            state = '' if not self.__state else 'and (not self.status[node])'
+            for node in self.queue:
+                if eval(f"(self.queue[node] {sign} self._top {state})"):
+                    self._top = self.queue[node]
+                    res = node
             del self.queue[res]
-            del self.status[res]
+            if self.__state:
+                del self.status[res]
+            self._top = float('inf') if self.type == 'min' else -float('inf')
             return res
-        else:
-            return None
+        return None
         
-    def update_state(self, node, value):
+    def set_state(self, node, value):
+        assert (value == True or value == False) and (self.__state)
         self.status[node] = value
     
 
-    def update_queue(self, node , value):
+    def enqueue(self, node , value):
         self.queue[node] = value
 
 
 
 if __name__ == "__main__":
     g = Graph()
-    for i in range(10):
+    for i in range(1,11):
         n = Node(i)
         g.add_node(n)
 
-    q = GraphPriorityQueue(g)
+    q = GraphPriorityQueue(g, type='min')
     for node in g:
-        val = random.randint(1,11)
-        q.update_queue(node,val)
+        val = random.randint(1,10)
+        q.enqueue(node,val)
 
     print(q)
     for _ in range(10):
