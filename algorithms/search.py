37eb60b7a224522a94be1243d05c60523abecd51
Fredpwol <cyborg290@gmail.com>
2020-07-08 15:17:49 +0100

    added dfs algorithm function

--- algorithms/search.py
+++ algorithms/search.py
@@ -3,28 +3,67 @@ from Pygnet.graph import Graph
 import queue
 
 
-def BFS(graph, source, key=None):
+def BFS(graph, source=None, key=None):
     assert type(graph) == Graph 
-    assert source in graph
+    source = graph[source]
 
     Q = queue.Queue(0)
     visisted = {}
-    level = {}
+    path = []
     for node in graph:
         visisted[node] = False
+    if source == None:
+        source = list(visisted.keys())[0]
     Q.put(source)
-    level[source] = 0
 
     while not Q.empty():
         node = Q.get()
+        path.append(node)
         for adj_node in node.adjacent_nodes:
             if not visisted[adj_node]:
                 Q.put(adj_node)
-                level[adj_node] = level[node] + 1
                 if adj_node == key:
-                    return {adj_node: level[adj_node]}
+                    return path
         visisted[node] = True
-    return level
+    return path
+
+
+
+def DFS(graph, source=None, key=None):
+    assert type(graph) == Graph 
+    source = graph[source]
+
+
+    def dfs_traverse(source, key=None, visisted=None):
+        if key:
+            if source == key:
+                return [source]
+            for adj_node in source.adjacent_nodes:
+                res = dfs_traverse(adj_node, key)
+                if res:
+                    res.append(source)
+                    return res
+            return None
+        else:
+            for node in source.adjacent_nodes:
+                if node in visisted:
+                    pass
+                dfs_traverse(node, visisted=visisted)
+            visisted.append(source)
+
+    if key != None: 
+        assert source != None
+        for adj_node in source.adjacent_nodes:
+            res = dfs_traverse(adj_node, key)
+            if res:
+                res.append(source)
+                return res
+    else:
+        for node in graph:
+            visisted =[]
+            dfs_traverse(node, visisted=visisted)
+        return visisted
+    return None
 
 
 
