ed4abbf8271695c13367465ce5b54c51463e7b6f
Fredpwol <cyborg290@gmail.com>
2020-07-08 22:44:13 +0100

    created test folder

---
+++ test/test.py
@@ -0,0 +1,57 @@
+
+import unittest
+from Pygnet import Node
+from Pygnet import Graph
+from Pygnet.algorithms.search import BFS
+import numpy as np
+from Pygnet.algorithms.sort import topological_sort
+from Pygnet.algorithms.path import dijkstra
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.n = [Node(1), Node(2), Node(3), Node(4)]
+        self.g = Graph()
+        for node in self.n :
+            self.g.add_node(node)
+        self.g.add_edge(1, 3)
+        self.g.add_edge(2, 4)
+
+    def test_adjecent_matrix(self):
+        shape = len(self.n)
+        test_res =  np.zeros((shape, shape))
+        test_res[0, 2] = 1
+        test_res[1, 3] = 1
+        test_res[2, 0] = 1
+        test_res[3, 1] = 1
+        res = self.g.graph_matrix == test_res
+        self.assertTrue(np.all(res))
+
+
+    def test_BFS(self):
+        self.assertEqual(BFS(self.g,1), [self.g[1], self.g[3]])
+
+
+    def test_iscyclic(self):
+        self.g = Graph(type='vector')
+        for i in range(5):
+            self.g.add_node(Node(i))
+        self.edge_list = [(0,1),(0,2),(1,4),(1,3),(2,1),(2,3),(3,0),(4,3)]
+        for x, y in self.edge_list:
+            self.g.add_edge(x, y)
+        self.assertTrue(self.g.is_cyclic())
+    
+
+    def test_topological_sort(self):
+        self.g = Graph(type='vector')
+        for i in range(6):
+            self.g.add_node(Node(i))
+        self.edge_list = [(5,2),(5,0),(4,0),(4,1),(2,3),(3,1)]
+        for x, y in self.edge_list:
+            self.g.add_edge(x, y)
+        sort = [node.value for node in topological_sort(self.g)]
+        self.assertEqual(sort , [4,5,2,0,3,1])
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/work.py
@@ -0,0 +1,15 @@
+from Pygnet import Graph, Node
+from Pygnet.algorithms.search import DFS
+from Pygnet.algorithms.path import dijkstra
+
+
+
+g = Graph(type='vector')
+for i in range(6):
+    g.add_node(Node(i))
+edges = [(0,1,2),(0,2,10),(1,3,10),(1,4,8),(2,5,2),(3,2,7),(4,5,5)]
+for e in edges:
+    g.add_edge(*e)
+print(g.graph_matrix)
+cost = dijkstra(g,0,5,path=True)
+print(cost)
