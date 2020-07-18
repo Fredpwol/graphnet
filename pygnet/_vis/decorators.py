8815800a5a5a7c7df16a8adfdcf89809ed634930
Fredpwol <cyborg290@gmail.com>
2020-07-18 22:39:47 +0100

    created preproccessor decorator for the plot functions

---
+++ pygnet/_vis/decorators.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+
+
+
+def preproccess_plot(func):
+
+    def wrapper(graph, ax, n, weighted):
+        space = np.linspace(0,1,n+1)
+        wrapper.scale = 100 // (n+1)
+        size = wrapper.scale + 10
+        x = []
+        for i in range(0, len(space)-1):
+            point = (space[i]+space[i+1]) / 2
+            x.append(point)
+        y = [np.random.random() for _ in range(n)]
+        for i, node in enumerate(graph):
+            wrapper.points[node] = (x[i], y[i])
+
+        func(graph, ax, n, weighted)
+        ax.plot(x, y, "o",markersize=size)
+
+        for node in wrapper.points:
+            x, y = wrapper.points[node]
+            value = graph.get_node_id(node)
+            ax.annotate(value, (x, y))
+    wrapper.scale = 0
+    wrapper.points = dict()
+    return wrapper
