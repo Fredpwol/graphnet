e3650e2743b750f7d5c68bee0a6728a4e44818a9
Fredpwol <cyborg290@gmail.com>
2020-07-18 22:36:11 +0100

    modified Node class docs and created docs, add repr for edge class

--- pygnet/components.py
+++ pygnet/components.py
@@ -4,14 +4,17 @@ class Node:
     The Node class is used for initializing a node in a graph
 
     Parameters
-        value : int, str or object 
-            This is the value of a node object. In graph it is used for identifying
-            every node in it.
+    ----------
+
+    value:int, str or object 
+        This is the value of a node object. In graph it is used for identifying
+        every node in it.
 
     Attributes
-        value : int, str or object 
-        adjecent_nodes : List(Node)
-            This is a list of all adjecent node object connected to the node.
+    ----------
+
+    adjacent_nodes : List(Node)
+        This is a list of all adjacent node object connected to the node.
     """
 
     def __init__(self, value, *args, **kwargs):
@@ -24,8 +27,9 @@ class Node:
         adds connected node in a graph to the adjecent_nodes list of the Node object.
 
         Parameters
-            node : Node
-                node to be added to the list.
+        ----------
+        node : Node
+            node to be added to the list.
         """
         self.adjacent_nodes.append(node)
 
@@ -36,8 +40,38 @@ class Node:
     
 
 class Edge:
+    """
+    Edge used for connecting two nodes together and serves as a link between them.
+
+    Parameters
+    ----------
+    _from:Node, str
+        The source point of the edge or the bottom of the edge.
+    _to:Node, str
+        The destination the edge stops.
+    weight:str, int, float,optional,default=None
+        This specifies the weights or label of the graph, by default is None
+        which means the edge is unweighted,
+        Note: to perfrom graph algorithm operations like pathfinding the weight 
+        most be either of type interger or float.
+    
+    Attributes
+    ----------
+    _from
+    _to
+    weight
+
+    """
 
     def __init__ (self, _from, _to, weight=None, *args, **kwargs):
         self.weight = weight
         self._from = _from
         self._to = _to
+
+    
+    def __repr__(self):
+        return "Edge(src=%s, dest=%s, weight=%s)"%(self._from, self._to, self.weight)
+
+
+
+
