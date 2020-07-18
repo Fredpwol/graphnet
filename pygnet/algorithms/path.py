934157c1b203c709255a852477d9bfc29e483ef7
Fredpwol <cyborg290@gmail.com>
2020-07-18 22:41:33 +0100

    created prims spanning tree function

--- pygnet/algorithms/path.py
+++ pygnet/algorithms/path.py
@@ -1,5 +1,6 @@
 from ..exceptions import GraphTypeError
 from ..utils import GraphPriorityQueue
+from ..graph import Graph
 
 
 def dijkstra(graph, _from, _to, path=False):
@@ -7,8 +8,7 @@ def dijkstra(graph, _from, _to, path=False):
         raise GraphTypeError("Graph contains circle.")
     elif graph.type != 'vector':
         raise GraphTypeError("Graph type most be vector not %s"%(graph.type))
-    
-    ref = graph.ref
+
     source = graph[_from]
     dist = {}
     for node in graph:
@@ -17,21 +17,69 @@ def dijkstra(graph, _from, _to, path=False):
         route = {}
         route[source] = [source]
     Q = GraphPriorityQueue(graph, type='min', state=True)
-    Q.enqueue(source)
+    Q.enqueue(source,0)
     dist[source] = 0
     while not Q.is_empty():
+        print(Q)
         node = Q.get()
-        node_id = node_id = eval("node.%s"%(ref))
+        node_id = graph.get_node_id(node)
         for neighbour in node.adjacent_nodes:
-            neigh_id = eval("neighbour.%s"%(ref))
+            neigh_id = graph.get_node_id(neighbour)
             if dist[neighbour] > dist[node] + graph.connections[node_id][neigh_id].weight:
                 dist[neighbour] = dist[node] + graph.connections[node_id][neigh_id].weight
                 if path:
                     route[neighbour] = route[node] + [neighbour]
+                Q.enqueue(neighbour, dist[neighbour])
         Q.set_state(node, True)
     to = graph[_to]
     res = dist[to] if not path else route[to]
 
     return res
 
-    
+#TODO Implement bellman ford algorithm,
+
+
+#TODO Implement minimum spanning tree
+def minimum_spanning_tree(graph):
+    if graph.type == "scalar":
+        key = {}
+        parent = {}
+        visited = []
+        tree = Graph()
+        for node in graph:
+            key[node] = float('inf')
+            parent[node] = None
+
+        source = graph.get_nodes[0]
+        key[source] = 0
+        visited.append(source)
+        for _ in range(len(graph)-1):
+            min_cost = float('inf')
+            dest = None
+            src = None
+            for node in visited:
+                for neigh in node.adjacent_nodes:
+                    cost = graph.connections[graph.get_node_id(node)][graph.get_node_id(neigh)].weight
+                    if cost < min_cost and neigh not in visited:
+                        min_cost = cost
+                        dest = neigh
+                        src = node
+            parent[dest] = src
+            visited.append(dest)
+            key[dest] = min_cost
+
+        for node in parent:
+            node.adjacent_nodes.clear()
+            tree.add_node(node)            
+        
+        for node in parent:
+            if parent[node] != None:
+                tree.add_edge(parent[node], node, key[node])
+        return tree
+        
+    else:
+        raise GraphTypeError("Graph type most be vector not %s"%(graph.type))
+
+
+
+
