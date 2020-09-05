81a8998a90546388765d584b3ac0ff8aa0e6cc43
Fredpwol <cyborg290@gmail.com>
2020-09-05 01:02:19 +0100

    created a plot_graph function to to stick with DRY

--- pygnet/_vis/layout.py
+++ pygnet/_vis/layout.py
@@ -8,17 +8,15 @@ COORDSA = "data"
 COORDSB = "data"
 
 
-
-
 @preproccess_plot
-def plot_graph_directed(graph, ax, n, weighted):
+def plot_graph(graph, ax, n, weighted, shrinkA, shrinkB, arrowstyle):
     for node in graph.get_nodes:
-        x_i, y_i = plot_graph_directed.points[node]
+        x_i, y_i = plot_graph.points[node]
         for adj_node in node.adjacent_nodes:
-            x_prime, y_prime = plot_graph_directed.points[adj_node]
+            x_prime, y_prime = plot_graph.points[adj_node]
             con = ConnectionPatch((x_i, y_i), (x_prime, y_prime), COORDSA, COORDSB,
-                        arrowstyle="-|>", shrinkA=30, shrinkB=30,
-                        mutation_scale=plot_graph_directed.scale, fc="k")
+                        arrowstyle=arrowstyle, shrinkA=shrinkA, shrinkB=shrinkB,
+                        mutation_scale=plot_graph.scale, fc="k")
             if weighted:
                 x_mid , y_mid = (x_prime+x_i) / 2, (y_prime+y_i) / 2
                 _from = graph.get_node_id(node)
@@ -28,24 +26,13 @@ def plot_graph_directed(graph, ax, n, weighted):
             ax.add_artist(con)
 
 
+def plot_graph_directed(graph,ax,n, weighted):
+    plot_graph(graph, ax, n, weighted, 30, 30, "-|>" )
 
 
-@preproccess_plot
-def plot_graph_undirected(graph, ax, n, weighted):
-    for node in graph.get_nodes:
-        x_i, y_i = plot_graph_undirected.points[node]
-        for adj_node in node.adjacent_nodes:
-            x_prime, y_prime = plot_graph_undirected.points[adj_node]
-            con = ConnectionPatch((x_i, y_i), (x_prime, y_prime), COORDSA, COORDSB,
-                        arrowstyle="-", shrinkA=1, shrinkB=1,
-                        mutation_scale=plot_graph_undirected.scale, fc="k")
-            if weighted:
-                x_mid , y_mid = (x_prime+x_i) / 2, (y_prime+y_i) / 2
-                _from = graph.get_node_id(node)
-                _to = graph.get_node_id(adj_node)
-                weight = graph.connections[_from][_to].weight
-                ax.text(x_mid, y_mid, weight, fontsize=12)
-            ax.add_artist(con)
+
+def plot_graph_undirected(graph,ax,n, weighted):
+    plot_graph(graph, ax, n, weighted, 1, 1, "-" )
 
 
 
