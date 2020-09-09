122b015178df8d2b77aed187eaec39d155c926dd
Fredpwol <cyborg290@gmail.com>
2020-09-09 06:50:01 +0100

    wrote docs for functions and removed GraphPriority

--- pygnet/utils.py
+++ pygnet/utils.py
@@ -3,6 +3,18 @@ import copy
 
 
 def check_cycle(source, visited, rec_stack):
+    """
+    checks if a circle exists from the source node in
+    the graph.
+    Parameters
+    ----------
+    visited: list
+        list of all node already visited.
+    rec_stack: dict
+        A dictionary of all nodes mapped with boolean
+        values which is the state if a node have been
+        visited in the recursion stack.
+    """
     visited[source] = True
     rec_stack[source] = True
     for adj_node in source.adjacent_nodes:
@@ -19,6 +31,20 @@ def check_cycle(source, visited, rec_stack):
 
 
 def remove_all_loops(graph, inplace=False):
+    """
+    Removes all loop edges in the graphs
+    Parameters
+    ----------
+    graph: Graph
+    inplace: boolean
+        if true modifies the graph object passed in
+        else returns a new graph object
+    returns
+    -------
+    out: Graph
+        A copy of the graph passed in but without loops,
+        if inplace is False
+    """
     temp = copy.deepcopy(graph)
     graph = temp if not inplace else graph
     for node in graph.get_nodes:
@@ -28,64 +54,6 @@ def remove_all_loops(graph, inplace=False):
         return graph
 
 
-
-
-
-
-class GraphPriorityQueue:
-
-    def __init__(self, graph, type="min", state=False):
-        if type == 'min' or type == 'max':
-            self.type = type
-        else:
-            raise TypeError("type attribute most be 'min' or 'max'")
-        self.graph = graph
-        self._top = float('inf') if self.type == 'min' else -float('inf')       
-        self.queue = dict()
-        self.__state = state
-        for node in self.graph:
-            self.queue[node] = float('inf')
-        if self.__state:
-            self.status = dict()
-            for node in self.graph:
-                self.status[node] = False
-        
-
-    def __repr__(self):
-        return str(list(self.queue.values()))
-
-
-
-    def get(self):
-        if self.__state:
-            if all(self.status.values()):
-                return None
-        if len(self.queue) != 0:
-            sign = "<=" if self.type == "min" else ">=" if self.type == "max" else ''
-            state = '' if not self.__state else 'and (not self.status[node])'
-            for node in self.queue:
-                if eval("(self.queue[node] %s self._top %s)"%(sign, state)):
-                    self._top = self.queue[node]
-                    res = node
-            del self.queue[res]
-            if self.__state:
-                del self.status[res]
-            self._top = float('inf') if self.type == 'min' else -float('inf')
-            return res
-        return None
-        
-    def set_state(self, node, value):
-        assert (value == True or value == False) and (self.__state)
-        self.status[node] = value
-    
-
-    def enqueue(self, node , value=0):
-        self.queue[node] = value
-
-    def is_empty(self):
-        if len(self.queue) !=0:
-            return False
-        return True
 
 
 
