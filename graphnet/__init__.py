bf0777af2ddd40fa898134b7e50a1c8963813cc2
Fredpwol <cyborg290@gmail.com>
2020-09-21 02:08:39 +0100

    built release

--- graphnet/__init__.py
+++ graphnet/__init__.py
@@ -6,6 +6,7 @@ it also comes with some usefull graph algorithm.
 """
 
 from __future__ import absolute_import
+import os
 
 VECTOR = "vector"
 SCALAR = "scalar"
@@ -14,5 +15,5 @@ from .components import Node, Edge
 from .graph import Graph, GraphPriorityQueue
 
 __author__ = "Fredrick Pwol"
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 __all__ = ["Graph", "GraphPriorityQueue"]
--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
 import pathlib
+import os
 
 here = pathlib.Path(__file__).parent.resolve()
 
-long_description = (here / 'README.md').read_text(encoding='utf-8')
+long_description = (here / 'README_pypi.md').read_text(encoding='utf-8')
 
 
 setup(
 
     name='graphnet',
-    version='0.1.0',
+    version=os.environ['GRAPHNET_VERSION'],
     description='A python library for graph manipulation and visualization',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -25,5 +26,6 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3 :: Only',
     ],
+    packages=find_packages(),
     install_requires=['numpy', "matplotlib"],
 )
